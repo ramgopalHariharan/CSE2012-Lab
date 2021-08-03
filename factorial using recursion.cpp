b7fd0aa169facc8437c31b7ce4ff252f7530137f
ramgopalHariharan <88359184+ramgopalHariharan@users.noreply.github.com>
2021-08-03 09:05:27 +0530

    Create factorial using recursion.cpp

---
+++ factorial using recursion.cpp
@@ -0,0 +1,25 @@
+/*
+Ramgopal Hariharan
+20BCE0249
+ramgopal.hariharan2020@vitstudent.ac.in
+*/
+
+//Factorial Using Recursion
+
+#include<iostream>
+#define MAXSIZE 100
+
+using namespace std;
+
+long factorial(int number) {
+  if(number==1) return 1;
+  else return number*factorial(number-1)
+}
+
+int main() {
+	int x;
+  cout<<"Enter Number:"
+	cin>>x;
+	cout<<"Factorial of x: "<<factorial(x);
+	return 0; 
+}
