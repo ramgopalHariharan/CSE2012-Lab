980ead15c565b4311c44e5b49e46ecb43c8497e8
ramgopalHariharan <88359184+ramgopalHariharan@users.noreply.github.com>
2021-08-11 16:47:12 +0530

    add comments

--- Assingment1/Q2.cpp
+++ Assingment1/Q2.cpp
@@ -9,8 +9,11 @@ int main() {
 	cin>>n;
 	cout<<"Enter Elements: ";
 	for(int i=0;i<n;i++) cin>>array[i];
+	
+	//Loop throught the array
 	for(int i=0;i<n-1;i++) {
 		for(int j=i+1;j<n;j++) {
+			//Check the number of elements that are less than the current element for each element
 			if(array[i]>array[j]) {
 				count++;
 				printf("(%d, %d)\n", array[i], array[j]);
