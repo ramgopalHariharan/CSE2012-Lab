2c99914c3b8cbb29ec93db41830df69d9a8907ec
ramgopalHariharan <88359184+ramgopalHariharan@users.noreply.github.com>
2021-08-11 16:43:15 +0530

    add comments

--- Assingment1/Q3.cpp
+++ Assingment1/Q3.cpp
@@ -2,28 +2,27 @@
 
 using namespace std;
 
-int findPoint(int a[], int n, bool order) {
-	int i=0;
-	if(order) {
-		while(a[i]>a[i+1]) i++;
-	}
-	else {
-		while(a[i]<a[i+1]) i++;
-	}
-	int split = i;
-	if(order) {
-		while(i<n) {
-			if(a[i]>a[i+1]) return -1;
-			i++;
-		}
-	}
-	else {
-		while(i<n) {
-			if(a[i]<a[i+1]) return -1;
-			i++;
-		}
-	}
-	return split;
+//Both functions return value of splitting point k if found
+// else they return -1
+
+//Check whether it increases and then decreases
+int ascendingFindPoint(int a[], int n) {
+	int i=0, j=n-1;
+	//check if array is in ascending order from both sides
+	while(a[i]<a[i+1]) i++;
+	while(a[j]<a[j-1]) j--;
+	if (i == j) return i;
+	else return -1;
+}
+
+//Check whether array decreases and then increases
+int descendingFindPoint(int a[], int n) {
+	int i=0, j=n-1;
+	//Check whether array is in descending order from both sides
+	while(a[i]>a[i+1]) i++;
+	while(a[j]>a[j-1]) j--;
+	if (i == j) return i;
+	else return -1;
 }
 
 int main() {
@@ -32,11 +31,19 @@ int main() {
 	cin>>n;
 	cout<<"Enter Elements: ";
 	for(int i=0;i<n;i++) cin>>array[i];
-	int split = findPoint(array, n, array[0]>array[1]);
+	int split = ascendingFindPoint(array, n);
+	//if one of the functions return -1, check the other function
 	if(split == -1) {
-		cout<<"No Point found";
+		split = descendingFindPoint(array, n);
+		//if both return -1, No value of k is found
+		if(split == -1) {
+			cout<<"No Split Point (k) Found";
+		}
+		else {
+			cout<<"Split point k = "<<split;
+		}
 	}
 	else {
-		cout<<"Split point = "<<split;
+		cout<<"Split point k = "<<split;
 	}
 }
