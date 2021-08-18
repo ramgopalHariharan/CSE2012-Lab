1d93c9afe9a0839319bc4fc5991cf23b66959469
ramgopalHariharan <88359184+ramgopalHariharan@users.noreply.github.com>
2021-08-18 16:58:21 +0530

    Update MinMaxIterative.cpp

--- Assingment1/MinMaxIterative.cpp
+++ Assingment1/MinMaxIterative.cpp
@@ -5,16 +5,26 @@ using namespace std;
 
 void MinMaxIterative(int a[], int n) {
 	int max = -32767, min = 327267;
+	int count = 0;
 	for(int i=0;i<n;i++) {
+		count++;
+		//If current element is greater than the maximum value, 
+		//change the maximum value
 		if(a[i] > max) {
+			
 			max = a[i];
 		}
+		count++;
+		//If current element is lesser than the minimum value,
+		//change the minimum value
 		if(a[i] < min) {
+			
 			min = a[i];
 		}
 	}
 	cout<<"Minimum Element = "<<min<<endl;
 	cout<<"Maximum Element = "<<max<<endl;
+	cout<<"Number of Comparisons = "<<count<<endl;
 }
 
 void GenerateArray(int a[], int n) {
@@ -31,12 +41,10 @@ void Display(int a[], int n) {
 
 int main() {
 	cout<<"Enter Number of elements: ";
-	int n, a[1000];
+	int n, a[10000];
 	cin>>n;
 	GenerateArray(a, n);
 	Display(a, n);
 	MinMaxIterative(a, n);
 	return 0;
 }
-
-
