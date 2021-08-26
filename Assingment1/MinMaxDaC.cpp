0615e768d7d23373b19c30f8c51d3dadb1ef7f86
ramgopalHariharan <88359184+ramgopalHariharan@users.noreply.github.com>
2021-08-26 10:23:02 +0530

    Update MinMaxDaC.cpp

--- Assingment1/MinMaxDaC.cpp
+++ Assingment1/MinMaxDaC.cpp
@@ -3,54 +3,27 @@
 
 using namespace std;
 
-//Recursively find the Minimum element using Divide and Conquer
-int MinDaC(int a[], int start, int end, int& count) {
-	count++;
-	//When the array size is one, retrun the element
-	if(start == end) {
-		return a[start];
-	}
-	else {
-		int mid = (end-start)/2;
-		//Split the arrray into left and right sub arrays 
-		//and find the minimum of each sub array
-		int left = MinDaC(a, start, start+mid, count);
-		int right = MinDaC(a, start+mid + 1, end, count);
-		//return the overall minimum of left and right sub arrays
-		int min = (left <= right)? left:right;
-		count++;
-		return min;
-	}
-}
-
-//Recursively find the Maximum element using Divide and Conquer
-int MaxDaC(int a[], int start, int end, int& count) {
-	count++;
-	//When the array size is one, retrun the element
-	if(start == end) {
-		return a[start];
-	}
-	else {
-		int mid = (end-start)/2;
-		//Split the arrray into left and right sub arrays 
-		//and find the maximum of each sub array
-		int left = MaxDaC(a, start, start+mid, count);
-		int right = MaxDaC(a, start+mid + 1, end, count);
-		//return the overall maximum of left and right sub arrays
-		int max = (left >= right)? left:right;
-		count++;
-		return max;
-	}
-}
-
-//Call both min and max DaC functions, in constant time
-int MinMaxDaC(int a[], int n) {
-	int count = 0;
-	int min = MinDaC(a, 0, n-1, count);
-	int max = MaxDaC(a, 0, n-1, count);
-	cout<<"Minimum Element: "<<min<<endl;
-	cout<<"Maximum Element: "<<max<<endl;
-	cout<<"Number of Comparisons = "<<count<<endl;
+void MinMaxDaC(int a[],int i,int j, int& count, int& max, int& min) {
+	if (i==j){ max = min = a[i];} //One Element Base Case
+	else if (i==j-1) // Two Elements Base Case
+    {
+    	count++;
+        if (a[i] < a[j]) {max = a[j]; min = a[i];}
+        else {max = a[i]; min = a[j];}
+    }
+    else
+    {
+        // Find where to split the set.
+        int max1 = -32767, min1 = 32767;
+		int mid = ( i + j )/2;
+        // Maximum and Minimum of the two SubProblens
+        MinMaxDaC(a, i, mid, count, max, min );
+        MinMaxDaC(a, mid+1, j, count, max1, min1 );
+        // Combine the solutions of the SubProblems.
+        count+=2;
+        if (max < max1) max = max1;
+        if (min > min1) min = min1;
+    }
 }
 
 void GenerateArray(int a[], int n) {
@@ -71,6 +44,10 @@ int main() {
 	cin>>n;
 	GenerateArray(a, n);
 	Display(a, n);
-	MinMaxDaC(a, n);
+	int min = 32767, max = -32767, count = 0;
+	MinMaxDaC(a, 0, n-1, count, max, min);
+	cout<<"Maximum Element = "<<max<<endl;
+	cout<<"Minimum Element = "<<min<<endl;
+	cout<<"Number of Comparisons = "<<count;
 	return 0;
 }
